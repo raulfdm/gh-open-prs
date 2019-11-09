f5fe214c07004cb5c5c2de428da29bfc5eb2c7e8
Raul de Melo <melo.raulf@gmail.com>
2019-11-09 18:56:06 +0100

    Add styled-components and load css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
-// next.config.js
-module.exports = {
+
+const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
+
+const nextConfig = {
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN
   }
 };
+
+module.exports = withPlugins([withCSS({})], nextConfig);
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: React.ReactNode) => (props: any) =>
+            //@ts-ignore
+            sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
