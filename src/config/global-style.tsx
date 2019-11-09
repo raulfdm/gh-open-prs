164c1e651d1cb014e27690e8a238848884a6f2ea
Raul de Melo <melo.raulf@gmail.com>
2019-11-09 18:56:28 +0100

    create global style and imports normalize

---
+++ src/config/global-style.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+import "normalize.css";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #252628;
+   
+  }
+
+  body{
+    color: #f7f8f8;
+  }
+`;
